7817c88bcdcb59a6bcbbfabcbfb6df7678809a71
yangzeran <yangzeran@rzon.tech>
2022-06-21 17:25:56 +0800

    test

--- WinFormsApp-Test/WinFormsApp-Test/Form1.cs
+++ WinFormsApp-Test/WinFormsApp-Test/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp_Test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
